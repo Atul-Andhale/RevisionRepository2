db2b77b14792ec0c89b7bf3b210fe7e992a3dc77
atulandhale37 <atulandhale37@gmail.com>
2023-07-15 09:42:26 +0530

    Project xyz first change commit

--- src/classwork/Assignment.java
+++ src/classwork/Assignment.java
@@ -5,7 +5,7 @@ public class Assignment {
 	public static void main(String[] args) 
 	{
 		int i,j;
-		
+//		int i,j;
 		for(i=1;i<=7;i=i+2) 
 		{
 		    for(j=7-i;j>=1;j--) 
--- src/patternPractice/Eg1.java
+++ src/patternPractice/Eg1.java
@@ -4,7 +4,7 @@ public class Eg1 {
 
 	public static void main(String[] args) {
 		// verical 
-		
+//		vertical 2
 		for(int i=1;i<=5;i++) 
 		{
 			System.out.println("*");
